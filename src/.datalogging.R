0201dba5c2c1dc8e6e00de2cbc277f5eaf2827a5
kai.mertens@tuhh.de <kai.mertens@tuhh.de>
2019-11-12 10:38:56 +0100

    datalogging

--- src/.datalogging.R
+++ src/.datalogging.R
@@ -8,22 +8,46 @@
   NUMB_PRO = FIRM$PRODUCTION_ENVIRONMENT$NUMB_PRO
   
   PRODUCT <- vector()
+  DENS <- vector()
+  RCC_VAR <- vector()
+  CP <- vector()
+  Error <-vector()
+  NUMB_Error <-vector()
+  CC <- vector()
+  MISCPOOLSIZE <- vector()
   RUN <- vector()
   DESIGN <- vector ()
+  PCb <- vector()
+  PCh <- vector()
+  Q <- vector()
+  
   
   
   
   PRODUCT <- c(PRODUCT, 1:NUMB_PRO) #How many products per run 
   DENS[PRODUCT] = FIRM$PRODUCTION_ENVIRONMENT$DENS #Scaling firm parameter to products.
+  Q_VAR[PRODUCT] = FIRM$PRODUCTION_ENVIRONMENT$Q_VAR #Scaling firm parameter to products.
+  RCC_VAR[PRODUCT] = FIRM$COSTING_SYSTEM$RC_VAR #Scaling firm parameter to products.
+  CP[PRODUCT] = FIRM$COSTING_SYSTEM$CP #Scaling firm parameter to products.
+  Error[PRODUCT] = FIRM$COSTING_SYSTEM$Error #Scaling firm parameter to products.
+  NUMB_Error[PRODUCT] = FIRM$COSTING_SYSTEM$NUMB_Error #Scaling firm parameter to products.
+  CC[PRODUCT] = FIRM$COSTING_SYSTEM$CC
+  MISCPOOLSIZE[PRODUCT] = FIRM$COSTING_SYSTEM$MISCPOOLSIZE
   RUN[PRODUCT] = o #run, repetition
   DESIGN[PRODUCT] = nn #which kind of design? 
   
   
-  DATApre = data.frame(o,nn,DENS) # construct the dataframe 
+  PE = (FIRM$COSTING_SYSTEM$PCH - FIRM$COSTING_SYSTEM$PCB)/FIRM$COSTING_SYSTEM$PCB
+  APE = abs((FIRM$COSTING_SYSTEM$PCH - FIRM$COSTING_SYSTEM$PCB))/FIRM$COSTING_SYSTEM$PCB
+  PCb[PRODUCT] = FIRM$COSTING_SYSTEM$PCB
+  PCh[PRODUCT] = FIRM$COSTING_SYSTEM$PCH
+  Q[PRODUCT] = FIRM$PRODUCTION_ENVIRONMENT$DEMAND
+  
+  DATApre = data.frame(o,nn,PRODUCT,PCb,PCh,Q,PE,APE,DENS,Q_VAR,RCC_VAR,CP,Error,NUMB_Error,CC,MISCPOOLSIZE) # construct the dataframe 
   
   DATAp = rbind(DATAp,DATApre) #put it together
   
-  return(DATAp)
+  retrn(DATAp)
 }
 
 
